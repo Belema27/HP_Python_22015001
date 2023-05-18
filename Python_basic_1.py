c12a4eb4223546d574c04bc0e634859340b7878f
Belema27 <133768709+Belema27@users.noreply.github.com>
2023-05-18 11:15:54 +0100

    Update Python_basic_1.py

--- Python_basic_1.py
+++ Python_basic_1.py
@@ -1,1 +1,4 @@
-
+name=input(" what is your name\t")
+age=input("what is your age\t")
+number=input("what is your phone number\t")
+print("Hello {},you are {} years old with phone number{}".format(name,age,number))
