882f1df3c842b481467d4f68b98c2905767aa493
Belema27 <133768709+Belema27@users.noreply.github.com>
2023-05-22 13:06:56 +0100

    Create Flow_control_1.py

---
+++ Flow_control_1.py
@@ -0,0 +1,9 @@
+username= "user1"
+password= "Admin"
+user=input("enter username\t\t")
+password=input("enter password\t\t") 
+if user == username:
+if password == password:
+   print("login successful")
+else:
+    print"(username and password invalid credentials")
